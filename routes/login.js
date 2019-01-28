a0207cdbafd4cf39957eda74a5886ec5b4aea599
solidarik <ildar.gubaydullin@gmail.com>
2019-01-28 07:40:42 +0500

    Improved LocalStrategy for open any page with login

--- routes/login.js
+++ routes/login.js
@@ -1,31 +1,13 @@
-
 const passport = require('koa-passport');
+
+exports.get = async function(ctx, next) {
+  ctx.body = ctx.render('login');
+};
 
 exports.post = passport.authenticate(
   'local',
   {
-    successRedirect: '/',
-    failureRedirect: '/',
-    failureFlash: true // req.flash, better
-    // assignProperty: 'something' присвоить юзера в свойство req.something
-    //   - нужно для привязывания акков соц. сетей
-    // если не стоит, то залогинит его вызовом req.login(user),
-    //   - это поместит user.id в session.passport.user (если не стоит опция session:false)
-    //   - также присвоит его в req.user
-  }
-);
-
-
-/* FOR AJAX:
-router.post('/login/local', async function(ctx, next) {
-  const ctx = ctx; // wrapper for the context
-
-  // @see node_modules/koa-passport/lib/framework/koa.js for passport.authenticate
-  // it returns the middleware to delegate
-  const middleware = passport.authenticate('local', async function(err, user, info) {
-    // ...
+    failureFlash: true,
+    failureRedirect: '/login',
+    successReturnToOrRedirect: true,
   });
-
-  await middleware.call(ctx, next);
-
-});*/
