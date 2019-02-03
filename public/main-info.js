f92cd5c20fe6e6a8f87ed1a6887d765b780bfb53
codmonkdev <codemonkeydeveloper@mail.ru>
2019-02-03 18:24:52 +0400

    добавил лог загрузки

--- public/main-info.js
+++ public/main-info.js
@@ -20,18 +20,18 @@ var width;
 var height;
 
 var onClickDropDown = function (d) {
-	buildBubble(d,svg,projection,width);
+	buildBubble(d, svg, projection, width);
 };
 
 
-$( document ).ready(function() {
-    addComboBoxFromJson.addBootstrapDropDown(loadedData, "dropDownList", "id", "RusName", onClickDropDown);
+$(document).ready(function () {
+	addComboBoxFromJson.addBootstrapDropDown(loadedData, "dropDownList", "id", "RusName", onClickDropDown);
 });
 
 
-function buildBubble(ldata,svg,projection,width) {
+function buildBubble(ldata, svg, projection, width) {
 	document.getElementById("nameContainer").innerHTML = "";
-	document.getElementById("nameContainer").innerHTML = "<h4>"+ldata.RusName+"</h4>";
+	document.getElementById("nameContainer").innerHTML = "<h4>" + ldata.RusName + "</h4>";
 	if (typeof (ldata.listYear) === "undefined") {
 		d3.json(ldata.url, function (error, dataFromFile) {
 			if (error) console.log(error);
@@ -55,7 +55,7 @@ function buildBubble(ldata,svg,projection,width) {
 			} else if (ldata.jsonType == "SAMARA") {
 				mxval = flagCircleInMap.getMaxValue(ldata.dataFromFile);
 			}
-			var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_",mxval,width);
+			var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_", mxval, width);
 			if (ldata.jsonType == "UFA") {
 				flagCircleInMapLoc.addFlagCircleInMapNew();
 			} else if (ldata.jsonType == "SAMARA") {
@@ -77,7 +77,7 @@ function buildBubble(ldata,svg,projection,width) {
 					} else if (ldata.jsonType == "SAMARA") {
 						mxval = flagCircleInMap.getMaxValue(ldata.dataFromFile);
 					}
-					var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_",mxval,width);
+					var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_", mxval, width);
 					flagCircleInMapLoc.addFlagCircleInMapNew();
 				}
 			} else if (ldata.jsonType == "SAMARA") {
@@ -94,7 +94,7 @@ function buildBubble(ldata,svg,projection,width) {
 						mxval = flagCircleInMap.getMaxValue(ldata.dataFromFile);
 					}
 					var curDataYearFilter = addSlider.filterByYear(ldata.dataFromFile, listYear[h2]);
-					var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_",mxval,width,width);
+					var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_", mxval, width, width);
 					flagCircleInMapLoc.addFlagCircleInMap();
 				}
 			}
@@ -107,7 +107,7 @@ function buildBubble(ldata,svg,projection,width) {
 		} else if (ldata.jsonType == "SAMARA") {
 			curDataYearFilter = addSlider.filterByYear(ldata.dataFromFile, ldata.listYear[0]);
 		}
-		var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_",width);
+		var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_", width);
 		if (ldata.jsonType == "UFA") {
 			flagCircleInMapLoc.addFlagCircleInMapNew();
 		} else if (ldata.jsonType == "SAMARA") {
@@ -123,7 +123,7 @@ function buildBubble(ldata,svg,projection,width) {
 				label.attr("x", xScale(h)).text(ldata.listYear[h2]);
 
 				var curDataYearFilter = addSlider.filterByYearNew(ldata.dataFromFile, ldata.listYear[h2]);
-				var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_",width);
+				var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_", width);
 				flagCircleInMapLoc.addFlagCircleInMapNew();
 			}
 		} else if (ldata.jsonType == "SAMARA") {
@@ -135,7 +135,7 @@ function buildBubble(ldata,svg,projection,width) {
 				label.attr("x", xScale(h)).text(ldata.listYear[h2]);
 
 				var curDataYearFilter = addSlider.filterByYear(ldata.dataFromFile, ldata.listYear[h2]);
-				var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_",width);
+				var flagCircleInMapLoc = new flagCircleInMap(curDataYearFilter, svg, projection, "img_", width);
 				flagCircleInMapLoc.addFlagCircleInMap();
 			}
 		}
@@ -147,68 +147,74 @@ var url = "data/countries.json";
 var url2 = "data/data_new.json";
 
 function startApp() {
-    d3.json(url, function (error, countries) {
-        if (error) console.log(error);
-        d3.json(url2, function (error, places) {
-            if (error) console.log(error);
-    
-    
-            width = parseInt(d3.select("#mapContainer").style("width")),
-                height =  Math.round(width * 5 / 12);//parseInt(d3.select("#mapContainer").style("height"));
-            var scale0 = (width - 1) / 2 / Math.PI;
-            projection = d3.geoEquirectangular()
-                .scale([scale0]) // scale to fit group width;
-                .translate([width / 2, height / 2 + 50])// ensure centred in group
-                //.translate([0,0])// ensure centred in group
-                ;
-    
-    
-            svg = d3.select("div#mapContainer").append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                // .call(d3.zoom().on("zoom", function () {
-                // 	svg.attr("transform", d3.event.transform)
-                // }))
-                ;
-    
-            addCountries.addContries(countries.features, svg, projection);
-    
-            var addImageInPage = new AddImageInPage(svg, places, "iso2", "img_", "img/flags/", ".png");
-            addImageInPage.addImageInPage();
-    
-            var ldata = loadedData[7];
-    
-            buildBubble(ldata,svg,projection,width);
-    
-    
-            // var listYear = addSlider.getListYear(places);
-    
-            // var curDataYearFilter = addSlider.filterByYear(places, listYear[0]);
-            // var flagCircleInMap = new flCInMap.flagCircleInMap(curDataYearFilter, svg, projection, "img_");
-            // flagCircleInMap.addFlagCircleInMap();
-    
-            // var updateFunction = function (h, handle, label, xScale) {
-            // 	// update position and text of label according to slider scale
-            // 	var h2 = Number((h).toFixed(0));
-            // 	handle.attr("cx", xScale(h));
-    
-            // 	label.attr("x", xScale(h)).text(listYear[h2]);
-    
-            // 	var curDataYearFilter = addSlider.filterByYear(places, listYear[h2]);
-            // 	var flagCircleInMap = new flCInMap.flagCircleInMap(curDataYearFilter, svg, projection, "img_");
-            // 	flagCircleInMap.addFlagCircleInMap();
-            // }
-    
-            // addSlider.addSlider("vis", width, listYear, updateFunction);
-    
-    
-            // var playButton = d3.select("#play-button");
-            // playButton.on("click", function () {
-    
-            // });
-        })
-    
-    });
- 
+	console.time("load countries");
+	d3.json(url, function (error, countries) {
+		console.timeEnd("load countries");
+		if (error) console.log(error);
+
+		console.time("add countries");
+		width = parseInt(d3.select("#mapContainer").style("width")),
+			height = Math.round(width * 5 / 12);//parseInt(d3.select("#mapContainer").style("height"));
+		var scale0 = (width - 1) / 2 / Math.PI;
+		projection = d3.geoEquirectangular()
+			.scale([scale0]) // scale to fit group width;
+			.translate([width / 2, height / 2 + 50])// ensure centred in group
+			//.translate([0,0])// ensure centred in group
+			;
+
+
+		svg = d3.select("div#mapContainer").append("svg")
+			.attr("width", width)
+			.attr("height", height)
+			// .call(d3.zoom().on("zoom", function () {
+			// 	svg.attr("transform", d3.event.transform)
+			// }))
+			;
+
+		addCountries.addContries(countries.features, svg, projection);
+		console.timeEnd("add countries");
+		console.time("load places");
+		d3.json(url2, function (error, places) {
+			console.timeEnd("load places");
+			if (error) console.log(error);
+
+			var ldata = loadedData[7];
+			console.time("add buuble");
+			buildBubble(ldata, svg, projection, width);
+			console.timeEnd("add buuble");
+			
+			console.time("add image");
+			var addImageInPage = new AddImageInPage(svg, places, "iso2", "img_", "img/flags/", ".png");
+			addImageInPage.addImageInPage();
+			console.timeEnd("add image");
+			// var listYear = addSlider.getListYear(places);
+
+			// var curDataYearFilter = addSlider.filterByYear(places, listYear[0]);
+			// var flagCircleInMap = new flCInMap.flagCircleInMap(curDataYearFilter, svg, projection, "img_");
+			// flagCircleInMap.addFlagCircleInMap();
+
+			// var updateFunction = function (h, handle, label, xScale) {
+			// 	// update position and text of label according to slider scale
+			// 	var h2 = Number((h).toFixed(0));
+			// 	handle.attr("cx", xScale(h));
+
+			// 	label.attr("x", xScale(h)).text(listYear[h2]);
+
+			// 	var curDataYearFilter = addSlider.filterByYear(places, listYear[h2]);
+			// 	var flagCircleInMap = new flCInMap.flagCircleInMap(curDataYearFilter, svg, projection, "img_");
+			// 	flagCircleInMap.addFlagCircleInMap();
+			// }
+
+			// addSlider.addSlider("vis", width, listYear, updateFunction);
+
+
+			// var playButton = d3.select("#play-button");
+			// playButton.on("click", function () {
+
+			// });
+		})
+
+	});
+
 }
 
