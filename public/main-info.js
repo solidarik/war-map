2c1f34ad484faaba8d72fe711fbe3e07141f16eb
codmonkdev <codemonkeydeveloper@mail.ru>
2019-02-11 19:46:57 +0400

    Реализация console.time

--- public/main-info.js
+++ public/main-info.js
@@ -62,6 +62,33 @@ loadedData.push({ "id": "9", "EngName": "Ground forses", "RusName": "Сухоп�
 // 	});
 // });
 
+// console.time implementation for IE
+if(window.console && typeof(window.console.time) == "undefined") {
+    console.time = function(name, reset){
+        if(!name) { return; }
+        var time = new Date().getTime();
+        if(!console.timeCounters) { console.timeCounters = {}; }
+        var key = "KEY" + name.toString();
+        if(!reset && console.timeCounters[key]) { return; }
+            console.timeCounters[key] = time;
+        };
+
+    console.timeEnd = function(name){
+        var time = new Date().getTime();
+        if(!console.timeCounters) { return; }
+        var key = "KEY" + name.toString();
+        var timeCounter = console.timeCounters[key];
+        var diff;
+        if(timeCounter) {
+            diff = time - timeCounter;
+            var label = name + ": " + diff + "ms";
+            console.info(label);
+            delete console.timeCounters[key];
+        }
+        return diff;
+    };
+}
+
 var svg;
 var projection;
 var width;
