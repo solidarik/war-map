71c8af58f28c94b92daf5290da8201d0584869fb
MokrevAA@samnipineft.ru <MokrevAA@samnipineft.ru>
2019-02-26 16:32:13 +0400

    откопилированы js

--- public/addMapInfoDiagramm.js
+++ public/addMapInfoDiagramm.js
@@ -1,36 +1,36 @@
-class AddMapInfoDiagramm {
+"use strict";
 
-    constructor(divElement, data, width) {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var AddMapInfoDiagramm = function () {
+    function AddMapInfoDiagramm(divElement, data, width) {
+        _classCallCheck(this, AddMapInfoDiagramm);
+
         this.divElement = divElement;
         this.data = data;
         this.width = width;
     }
 
-    addMapInfoDiagrammInDiv() {
-        // set the dimensions and margins of the graph
-        var margin = { top: 20, right: 20, bottom: 30, left: 40 },
-            width = this.width - margin.left - margin.right,
-            height = Math.round(this.width*0.52) - margin.top - margin.bottom;
+    _createClass(AddMapInfoDiagramm, [{
+        key: "addMapInfoDiagrammInDiv",
+        value: function addMapInfoDiagrammInDiv() {
+            // set the dimensions and margins of the graph
+            var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+                width = this.width - margin.left - margin.right,
+                height = Math.round(this.width * 0.52) - margin.top - margin.bottom;
 
-        // set the ranges
-        var x = d3.scaleBand()
-            .range([0, width])
-            .padding(0.1);
-        var y = d3.scaleLinear()
-            .range([height, 0]);
+            // set the ranges
+            var x = d3.scaleBand().range([0, width]).padding(0.1);
+            var y = d3.scaleLinear().range([height, 0]);
 
-        // append the svg object to the body of the page
-        // append a 'group' element to 'svg'
-        // moves the 'group' element to the top left margin
-        d3.select("#"+this.divElement+"Svg").remove();
+            // append the svg object to the body of the page
+            // append a 'group' element to 'svg'
+            // moves the 'group' element to the top left margin
+            d3.select("#" + this.divElement + "Svg").remove();
 
-        var svg = d3.select("#"+this.divElement).append("svg")
-            .attr("id",this.divElement+"Svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
+            var svg = d3.select("#" + this.divElement).append("svg").attr("id", this.divElement + "Svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
             console.log("this.data=" + JSON.stringify(this.data));
 
@@ -39,64 +39,69 @@ class AddMapInfoDiagramm {
             // });
 
             // Scale the range of the data in the domains
-            console.log("this.data.map=" + this.data.map(function (d) { return d.date; }));
-            x.domain(this.data.map(function (d) { return d.date; }));
-            console.log("d3.max=" + d3.max(this.data, function (d) { return d.value; }));
-            y.domain([0, d3.max(this.data, function (d) { return d.value; })]);
+            console.log("this.data.map=" + this.data.map(function (d) {
+                return d.date;
+            }));
+            x.domain(this.data.map(function (d) {
+                return d.date;
+            }));
+            console.log("d3.max=" + d3.max(this.data, function (d) {
+                return d.value;
+            }));
+            y.domain([0, d3.max(this.data, function (d) {
+                return d.value;
+            })]);
 
             // append the rectangles for the bar chart
-            svg.selectAll(".bar")
-                .data(this.data)
-                .enter().append("rect")
-                .attr("class", "bar")
-                .attr("x", function (d) { return x(d.date); })
-                .attr("width", x.bandwidth())
-                .attr("y", function (d) { return y(d.value); })
-                .attr("height", function (d) { return height - y(d.value); });
+            svg.selectAll(".bar").data(this.data).enter().append("rect").attr("class", "bar").attr("x", function (d) {
+                return x(d.date);
+            }).attr("width", x.bandwidth()).attr("y", function (d) {
+                return y(d.value);
+            }).attr("height", function (d) {
+                return height - y(d.value);
+            });
 
             // add the x Axis
-            svg.append("g")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x));
+            svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
 
             // add the y Axis
-            svg.append("g")
-                .call(d3.axisLeft(y));
+            svg.append("g").call(d3.axisLeft(y));
 
+            // // get the data
+            // d3.csv("data/sales.csv", function (error, data) {
+            //     if (error) throw error;
 
-        // // get the data
-        // d3.csv("data/sales.csv", function (error, data) {
-        //     if (error) throw error;
+            //     // format the data
+            //     data.forEach(function (d) {
+            //         d.sales = +d.sales;
+            //     });
 
-        //     // format the data
-        //     data.forEach(function (d) {
-        //         d.sales = +d.sales;
-        //     });
+            //     // Scale the range of the data in the domains
+            //     x.domain(data.map(function (d) { return d.salesperson; }));
+            //     y.domain([0, d3.max(data, function (d) { return d.sales; })]);
 
-        //     // Scale the range of the data in the domains
-        //     x.domain(data.map(function (d) { return d.salesperson; }));
-        //     y.domain([0, d3.max(data, function (d) { return d.sales; })]);
+            //     // append the rectangles for the bar chart
+            //     svg.selectAll(".bar")
+            //         .data(data)
+            //         .enter().append("rect")
+            //         .attr("class", "bar")
+            //         .attr("x", function (d) { return x(d.salesperson); })
+            //         .attr("width", x.bandwidth())
+            //         .attr("y", function (d) { return y(d.sales); })
+            //         .attr("height", function (d) { return height - y(d.sales); });
 
-        //     // append the rectangles for the bar chart
-        //     svg.selectAll(".bar")
-        //         .data(data)
-        //         .enter().append("rect")
-        //         .attr("class", "bar")
-        //         .attr("x", function (d) { return x(d.salesperson); })
-        //         .attr("width", x.bandwidth())
-        //         .attr("y", function (d) { return y(d.sales); })
-        //         .attr("height", function (d) { return height - y(d.sales); });
+            //     // add the x Axis
+            //     svg.append("g")
+            //         .attr("transform", "translate(0," + height + ")")
+            //         .call(d3.axisBottom(x));
 
-        //     // add the x Axis
-        //     svg.append("g")
-        //         .attr("transform", "translate(0," + height + ")")
-        //         .call(d3.axisBottom(x));
+            //     // add the y Axis
+            //     svg.append("g")
+            //         .call(d3.axisLeft(y));
 
-        //     // add the y Axis
-        //     svg.append("g")
-        //         .call(d3.axisLeft(y));
+            // });
+        }
+    }]);
 
-        // });
-    }
-
-}
+    return AddMapInfoDiagramm;
+}();
--- public/addSlider.js
+++ public/addSlider.js
@@ -1,67 +1,83 @@
-class addSlider {
+'use strict';
 
-	static getListYear(data) {
-		var listYear = [];
-		for (var d of data) {
-			d.dataCountries.forEach(function (d1) {
-				var elem = d1.year;
-				while (elem.length < 4) {
-					elem = '0' + elem;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var addSlider = function () {
+	function addSlider() {
+		_classCallCheck(this, addSlider);
+	}
+
+	_createClass(addSlider, null, [{
+		key: 'getListYear',
+		value: function getListYear(data) {
+			var listYear = [];
+			var _iteratorNormalCompletion = true;
+			var _didIteratorError = false;
+			var _iteratorError = undefined;
+
+			try {
+				for (var _iterator = data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+					var d = _step.value;
+
+					d.dataCountries.forEach(function (d1) {
+						var elem = d1.year;
+						while (elem.length < 4) {
+							elem = '0' + elem;
+						}
+						listYear.push(elem);
+					});
+					if (listYear.length > 0) {
+						break;
+					}
 				}
-				listYear.push(elem);
-			});
-			if (listYear.length > 0) {
-				break;
+			} catch (err) {
+				_didIteratorError = true;
+				_iteratorError = err;
+			} finally {
+				try {
+					if (!_iteratorNormalCompletion && _iterator.return) {
+						_iterator.return();
+					}
+				} finally {
+					if (_didIteratorError) {
+						throw _iteratorError;
+					}
+				}
 			}
-		};
-		return listYear;
-	}
 
-	static getListYearNew(data) {
-		var listYear = [];
+			;
+			return listYear;
+		}
+	}, {
+		key: 'getListYearNew',
+		value: function getListYearNew(data) {
+			var listYear = [];
 
-		// var fdata = data.filter(function (n) {
-		// 	return n.iso3 === "RUS";
-		// });
-		// console.log("fdata=" + fdata);
-		// for (var index = 0; index < data.length; index++) {
-		// 	const element = data[index].date;
-		// 	if(element == "1945"){
-		// 		console.log("(element == 1945");
-		// 		console.log("listYear="+listYear);
-		// 		console.log("listYear.includes(element)="+listYear.includes(element));
-		// 		if(!listYear.includes(element)){
-		// 			listYear.push(element);
-		// 		}
-		// 	}else{
-		// 		if(!listYear.includes(element)){
-		// 			listYear.push(element);
-		// 		}
-		// 	}
+			// var fdata = data.filter(function (n) {
+			// 	return n.iso3 === "RUS";
+			// });
+			// console.log("fdata=" + fdata);
+			// for (var index = 0; index < data.length; index++) {
+			// 	const element = data[index].date;
+			// 	if(element == "1945"){
+			// 		console.log("(element == 1945");
+			// 		console.log("listYear="+listYear);
+			// 		console.log("listYear.includes(element)="+listYear.includes(element));
+			// 		if(!listYear.includes(element)){
+			// 			listYear.push(element);
+			// 		}
+			// 	}else{
+			// 		if(!listYear.includes(element)){
+			// 			listYear.push(element);
+			// 		}
+			// 	}
 
-		// }
+			// }
 
-		data.forEach(function (d) {
-			var elem = d.date;
-			while (elem.length < 4) {
-				elem = '0' + elem;
-			}
-			//if (!listYear.includes(elem)) {
-			if (listYear.indexOf(elem) == -1) {
-				listYear.push(elem);
-			}
-		});
-
-		listYear.sort();
-
-		return listYear;
-	}
-
-	static getListYearNewArray(data) {
-		var listYear = [];
-		data.forEach(function (d) {
-			d.forEach(function (d1) {
-				var elem = d1.date;
+			data.forEach(function (d) {
+				var elem = d.date;
 				while (elem.length < 4) {
 					elem = '0' + elem;
 				}
@@ -70,119 +86,122 @@ class addSlider {
 					listYear.push(elem);
 				}
 			});
-		});
 
-		listYear.sort();
+			listYear.sort();
 
-		return listYear;
-	}
+			return listYear;
+		}
+	}, {
+		key: 'getListYearNewArray',
+		value: function getListYearNewArray(data) {
+			var listYear = [];
+			data.forEach(function (d) {
+				d.forEach(function (d1) {
+					var elem = d1.date;
+					while (elem.length < 4) {
+						elem = '0' + elem;
+					}
+					//if (!listYear.includes(elem)) {
+					if (listYear.indexOf(elem) == -1) {
+						listYear.push(elem);
+					}
+				});
+			});
 
-	static filterByYear(data, yearFilter) {
-		var dataYearFilter = [];
-		data.forEach(function (d) {
-			var curElemnt = {};
-			curElemnt.id = d.id;
-			curElemnt.name = d.name;
-			curElemnt.name_ru = d.name_ru;
-			curElemnt.fips = d.fips;
-			curElemnt.iso2 = d.iso2;
-			curElemnt.iso3 = d.iso3;
-			curElemnt.centroid = d.centroid;
-			curElemnt.dataCountries = d.dataCountries.filter(function (object) {
-				var year = object.year;
+			listYear.sort();
+
+			return listYear;
+		}
+	}, {
+		key: 'filterByYear',
+		value: function filterByYear(data, yearFilter) {
+			var dataYearFilter = [];
+			data.forEach(function (d) {
+				var curElemnt = {};
+				curElemnt.id = d.id;
+				curElemnt.name = d.name;
+				curElemnt.name_ru = d.name_ru;
+				curElemnt.fips = d.fips;
+				curElemnt.iso2 = d.iso2;
+				curElemnt.iso3 = d.iso3;
+				curElemnt.centroid = d.centroid;
+				curElemnt.dataCountries = d.dataCountries.filter(function (object) {
+					var year = object.year;
+					return year == yearFilter;
+				});
+				dataYearFilter.push(curElemnt);
+			});
+			return dataYearFilter;
+		}
+	}, {
+		key: 'filterByIso3',
+		value: function filterByIso3(data, iso3Filter) {
+			var dataIso3Filter = data.filter(function (object) {
+				var iso3 = object.iso3;
+				return iso3 == iso3Filter;
+			});
+			return dataIso3Filter;
+		}
+	}, {
+		key: 'filterByYearNew',
+		value: function filterByYearNew(data, yearFilter) {
+			var dataYearFilter = data.filter(function (object) {
+				var year = object.date;
 				return year == yearFilter;
 			});
-			dataYearFilter.push(curElemnt);
-		});
-		return dataYearFilter;
-	}
-
-	static filterByIso3(data, iso3Filter) {
-		var dataIso3Filter = data.filter(function (object) {
-			var iso3 = object.iso3;
-			return iso3 == iso3Filter;
-		});
-		return dataIso3Filter;
-	}
-
-	static filterByYearNew(data, yearFilter) {
-		var dataYearFilter = data.filter(function (object) {
-			var year = object.date;
-			return year == yearFilter;
-		});
-		return dataYearFilter;
-	}
-
-	static filterByIso3New(data, iso3Filter) {
-		var dataIso3Filter = data.filter(function (object) {
-			var iso3 = object.iso3;
-			return iso3 == iso3Filter;
-		});
-		return dataIso3Filter;
-	}
-
-	static addSlider(idElemForSvg, width, listYear, updateFunction) {
-		var margin = { top: 75, right: 50, bottom: 0, left: 50 };
-		var widthSlider = width - margin.left - margin.right,
-			heightSlider = 150;
-
-		var xScale = d3.scaleLinear()
-			.domain([0, listYear.length - 1])
-			.range([0, widthSlider])
-			.clamp(true);
-
-		d3.select("#" + idElemForSvg).selectAll("svg").remove();
-
-		var svg = d3.select("#" + idElemForSvg)
-			.append("svg")
-			.attr("width", widthSlider + margin.left + margin.right)
-			.attr("height", heightSlider);
-
-
-		var slider = svg.append("g")
-			.attr("class", "slider")
-			.attr("transform", "translate(" + margin.left + "," + heightSlider / 4 + ")");
-
-		slider.append("line")
-			.attr("class", "track")
-			.attr("x1", function () { return xScale.range()[0]; })
-			.attr("x2", function () { return xScale.range()[1]; })
-			.select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
-			.attr("class", "track-inset")
-			.select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
-			.attr("class", "track-overlay")
-			.call(d3.drag()
-				.on("start.interrupt", function () { slider.interrupt(); })
-				.on("start drag", function () {
-					updateFunction(xScale.invert(d3.event.x), handle, label, xScale);
-				})
-			);
-
-		var yearCount = listYear.length;
-		if (yearCount > Math.abs(widthSlider / 40)) {
-			yearCount = Math.abs(widthSlider / 40);
+			return dataYearFilter;
 		}
-		slider.insert("g", ".track-overlay")
-			.attr("class", "ticks")
-			.attr("transform", "translate(0," + 18 + ")")
-			.selectAll("text")
-			.data(xScale.ticks(yearCount))
-			.enter()
-			.append("text")
-			.attr("x", xScale)
-			.attr("y", 10)
-			.attr("text-anchor", "middle")
-			.text(function (d) { return listYear[d]; });
+	}, {
+		key: 'filterByIso3New',
+		value: function filterByIso3New(data, iso3Filter) {
+			var dataIso3Filter = data.filter(function (object) {
+				var iso3 = object.iso3;
+				return iso3 == iso3Filter;
+			});
+			return dataIso3Filter;
+		}
+	}, {
+		key: 'addSlider',
+		value: function addSlider(idElemForSvg, width, listYear, updateFunction) {
+			var margin = { top: 75, right: 50, bottom: 0, left: 50 };
+			var widthSlider = width - margin.left - margin.right,
+			    heightSlider = 150;
 
-		var handle = slider.insert("circle", ".track-overlay")
-			.attr("class", "handle")
-			.attr("r", 9);
+			var xScale = d3.scaleLinear().domain([0, listYear.length - 1]).range([0, widthSlider]).clamp(true);
 
-		var label = slider.append("text")
-			.attr("class", "label")
-			.attr("text-anchor", "middle")
-			.text(listYear[0])
-			.attr("transform", "translate(0," + (-15) + ")")
-	}
+			d3.select("#" + idElemForSvg).selectAll("svg").remove();
 
-}
+			var svg = d3.select("#" + idElemForSvg).append("svg").attr("width", widthSlider + margin.left + margin.right).attr("height", heightSlider);
+
+			var slider = svg.append("g").attr("class", "slider").attr("transform", "translate(" + margin.left + "," + heightSlider / 4 + ")");
+
+			slider.append("line").attr("class", "track").attr("x1", function () {
+				return xScale.range()[0];
+			}).attr("x2", function () {
+				return xScale.range()[1];
+			}).select(function () {
+				return this.parentNode.appendChild(this.cloneNode(true));
+			}).attr("class", "track-inset").select(function () {
+				return this.parentNode.appendChild(this.cloneNode(true));
+			}).attr("class", "track-overlay").call(d3.drag().on("start.interrupt", function () {
+				slider.interrupt();
+			}).on("start drag", function () {
+				updateFunction(xScale.invert(d3.event.x), handle, label, xScale);
+			}));
+
+			var yearCount = listYear.length;
+			if (yearCount > Math.abs(widthSlider / 40)) {
+				yearCount = Math.abs(widthSlider / 40);
+			}
+			slider.insert("g", ".track-overlay").attr("class", "ticks").attr("transform", "translate(0," + 18 + ")").selectAll("text").data(xScale.ticks(yearCount)).enter().append("text").attr("x", xScale).attr("y", 10).attr("text-anchor", "middle").text(function (d) {
+				return listYear[d];
+			});
+
+			var handle = slider.insert("circle", ".track-overlay").attr("class", "handle").attr("r", 9);
+
+			var label = slider.append("text").attr("class", "label").attr("text-anchor", "middle").text(listYear[0]).attr("transform", "translate(0," + -15 + ")");
+		}
+	}]);
+
+	return addSlider;
+}();
