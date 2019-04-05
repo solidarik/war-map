a92530d941809abe28468f17d964e43ae421fce8
codmonkdev <codemonkeydeveloper@mail.ru>
2019-04-05 13:55:40 +0400

    Добавил новыйе файлы для стартовой страницы

---
+++ public/js/scroll.js
@@ -0,0 +1,36 @@
+﻿var box;
+
+$(document).ready(function () {
+    $('#wrapper').scrollTo($('#box1'), 800);
+
+    $(".content").resizable({ ghost: true });
+
+    $(window).bind('resize', function (event) {
+        window.resizeEvt;
+        $(window).resize(function () {
+            clearTimeout(window.resizeEvt);
+            window.resizeEvt = setTimeout(function () {
+                if (box !== null) {
+                    $('#wrapper').scrollTo(box, 1);
+                    //box = null;
+                }
+            }, 250);
+        });
+    });
+
+//    $(window).bind('resize', function (event) {
+//        if (!$(event.target).hasClass('ui-resizable')) {
+//            if (box !== null) {
+//                $('#wrapper').scrollTo(box, 800);
+//                box = null;
+//            }
+//
+//        }
+//    });
+
+    $('a.link').click(function () {
+        box = $(this).attr('href');
+        $('#wrapper').scrollTo($(this).attr('href'), 800);
+        return false;
+    });
+});
