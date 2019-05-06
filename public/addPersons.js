2fa2dad286e2b8cff84aa3780992dd4aed5a4b68
MokrevAA@samnipineft.ru <MokrevAA@samnipineft.ru>
2019-05-06 16:03:39 +0400

    Добавлено заполнение личностей

--- public/addPersons.js
+++ public/addPersons.js
@@ -10,6 +10,7 @@ var addPersons = function () {
 
         this.idTable = idTable;
         this.fileUrl = fileUrl;
+        this.data = null;
     }
 
     _createClass(addPersons, [{
@@ -21,56 +22,108 @@ var addPersons = function () {
         }
     }, {
         key: "rowTableClickHandler",
-        value: function rowTableClickHandler() {
-            $(this).addClass("event-active-row");
-            $(this).siblings().removeClass("event-active-row");
-            console.log("clicked");
+        value: function rowTableClickHandler(thisThis, thisTr) {
+            $(thisTr).addClass("event-active-row");
+            $(thisTr).siblings().removeClass("event-active-row");
+            console.log("clicked " + $(thisTr).attr('id'));
+            var id = parseInt($(thisTr).attr("id"));
+            $('#FIO').html(thisThis.data[id].Surname + " " + thisThis.data[id].Name + " " + thisThis.data[id].MiddleName);
+            $('#LifeTime').html(thisThis.data[id].DateBirth + " - " + thisThis.data[id].DateDeath + "<br>" + thisThis.data[id].PlaceBirth);
+            $('#imgPerson').src = thisThis.data[id].PhotoUrl;
+            $('#imgPerson').attr('src', thisThis.data[id].PhotoUrl);
+            $('#description').html(thisThis.data[id].Description + " <a target='_blank' rel='noopener noreferrer' href='" + thisThis.data[id].Source + "'>" + "Подробнее...</a>");
+        }
+    }, {
+        key: "addDataToTable",
+        value: function addDataToTable() {
+            console.log("addDataTotable");
+
+            var obj = this.data;
+            this.clearTable();
+            var table = $("#" + this.idTable);
+            //table[0].border = "1";
+            var columns = Object.keys(obj[0]);
+            var columnCount = columns.length;
+            var row = $(table[0].insertRow(-1));
+            for (var i = 0; i < columnCount; i++) {
+                if (i == 0 || i == 1 || i == 2 || i == 5) {
+                    var headerCell = $("<th />");
+                    headerCell.html([columns[i]]);
+                    row.append(headerCell);
+                }
+            }
+
+            for (var i = 0; i < obj.length; i++) {
+                row = $(table[0].insertRow(-1));
+                row.addClass("hand-cursor");
+                row.attr("id", i);
+                for (var j = 0; j < columnCount; j++) {
+                    if (j == 0 || j == 1 || j == 2 || j == 5) {
+                        var cell = $("<td />");
+                        cell.html(obj[i][columns[j]]);
+                        row.append(cell);
+                    }
+                }
+            }
+
+            var thisThis = this;
+
+            document.querySelectorAll("#" + this.idTable + " tr").forEach(function (e) {
+                return e.addEventListener("click", function () {
+                    thisThis.rowTableClickHandler(thisThis, this);
+                });
+            });
         }
     }, {
         key: "fillTable",
         value: function fillTable() {
             console.log("fillTable");
 
-            var thisThis = this;
+            if (this.data == null) {
+                var thisThis = this;
 
-            console.time("load persons");
-            d3.json(this.fileUrl, function (error, persons) {
-                console.timeEnd("load persons");
-                if (error) console.log(error);
+                console.time("load persons");
+                d3.json(this.fileUrl, function (error, persons) {
+                    console.timeEnd("load persons");
+                    if (error) console.log(error);
 
-                var obj = persons;
-                thisThis.clearTable();
-                var table = $("#" + thisThis.idTable);
-                //table[0].border = "1";
-                var columns = Object.keys(obj[0]);
-                var columnCount = columns.length;
-                var row = $(table[0].insertRow(-1));
-                for (var i = 0; i < columnCount; i++) {
-                    if (i == 0 || i == 1 || i == 2 || i == 5) {
-                        var headerCell = $("<th />");
-                        headerCell.html([columns[i]]);
-                        row.append(headerCell);
-                    }
-                }
+                    thisThis.data = persons;
+                    thisThis.addDataToTable();
 
-                for (var i = 0; i < obj.length; i++) {
-                    row = $(table[0].insertRow(-1));
-                    row.addClass("hand-cursor");
-                    for (var j = 0; j < columnCount; j++) {
-                        if (j == 0 || j == 1 || j == 2 || j == 5) {
-                            var cell = $("<td />");
-                            cell.html(obj[i][columns[j]]);
-                            row.append(cell);
+                    /*var obj = persons;
+                    thisThis.clearTable();
+                    var table = $("#" + thisThis.idTable);
+                    //table[0].border = "1";
+                    var columns = Object.keys(obj[0]);
+                    var columnCount = columns.length;
+                    var row = $(table[0].insertRow(-1));
+                    for (var i = 0; i < columnCount; i++) {
+                        if (i == 0 || i == 1 || i == 2 || i == 5) {
+                            var headerCell = $("<th />");
+                            headerCell.html([columns[i]]);
+                            row.append(headerCell);
                         }
                     }
-                }
-
-                document.querySelectorAll("#" + thisThis.idTable + " tr").forEach(function (e) {
-                    return e.addEventListener("click", thisThis.rowTableClickHandler);
+                         for (var i = 0; i < obj.length; i++) {
+                        row = $(table[0].insertRow(-1));
+                        row.addClass("hand-cursor");
+                        row.id = i;
+                        for (var j = 0; j < columnCount; j++) {
+                            if (j == 0 || j == 1 || j == 2 || j == 5) {
+                                var cell = $("<td />");
+                                cell.html(obj[i][columns[j]]);
+                                row.append(cell);
+                            }
+                        }
+                    }
+                         document.querySelectorAll("#" + thisThis.idTable + " tr")
+                        .forEach(e => e.addEventListener("click", thisThis.rowTableClickHandler));
+                         //table.append(table);
+                    */
                 });
-
-                //table.append(table);
-            });
+            } else {
+                this.addDataToTable();
+            }
         }
     }]);
 
--- public/addPersons_src.js
+++ public/addPersons_src.js
@@ -3,6 +3,7 @@ class addPersons {
     constructor(idTable, fileUrl) {
         this.idTable = idTable;
         this.fileUrl = fileUrl;
+        this.data = null;
     }
 
     clearTable() {
@@ -11,55 +12,109 @@ class addPersons {
         dvTable.html("");
     }
 
-    rowTableClickHandler() {
-        $(this).addClass("event-active-row");
-        $(this).siblings().removeClass("event-active-row"); 
-        console.log("clicked")
+    rowTableClickHandler(thisThis,thisTr) {
+        $(thisTr).addClass("event-active-row");
+        $(thisTr).siblings().removeClass("event-active-row");
+        console.log("clicked " + $(thisTr).attr('id'));
+        var id = parseInt($(thisTr).attr("id"));
+        $('#FIO').html(thisThis.data[id].Surname+" "+thisThis.data[id].Name+" "+thisThis.data[id].MiddleName);
+        $('#LifeTime').html(thisThis.data[id].DateBirth + " - " + thisThis.data[id].DateDeath+"<br>"+thisThis.data[id].PlaceBirth);
+        $('#imgPerson').src=thisThis.data[id].PhotoUrl;
+        $('#imgPerson').attr('src',thisThis.data[id].PhotoUrl);
+        $('#description').html(thisThis.data[id].Description + " <a target='_blank' rel='noopener noreferrer' href='"+thisThis.data[id].Source+"'>"+"Подробнее...</a>");
+        
     }
-    
+
+    addDataToTable() {
+        console.log("addDataTotable");
+
+        var obj = this.data;
+        this.clearTable();
+        var table = $("#" + this.idTable);
+        //table[0].border = "1";
+        var columns = Object.keys(obj[0]);
+        var columnCount = columns.length;
+        var row = $(table[0].insertRow(-1));
+        for (var i = 0; i < columnCount; i++) {
+            if (i == 0 || i == 1 || i == 2 || i == 5) {
+                var headerCell = $("<th />");
+                headerCell.html([columns[i]]);
+                row.append(headerCell);
+            }
+        }
+
+        for (var i = 0; i < obj.length; i++) {
+            row = $(table[0].insertRow(-1));
+            row.addClass("hand-cursor");
+            row.attr("id", i);
+            for (var j = 0; j < columnCount; j++) {
+                if (j == 0 || j == 1 || j == 2 || j == 5) {
+                    var cell = $("<td />");
+                    cell.html(obj[i][columns[j]]);
+                    row.append(cell);
+                }
+            }
+        }
+
+        var thisThis = this;
+
+        document.querySelectorAll("#" + this.idTable + " tr")
+            .forEach(e => e.addEventListener("click", function () {thisThis.rowTableClickHandler(thisThis,this);}));
+
+    }
 
     fillTable() {
         console.log("fillTable");
 
-        var thisThis = this;
+        if (this.data == null) {
+            var thisThis = this;
 
-        console.time("load persons");
-        d3.json(this.fileUrl, function (error, persons) {
-            console.timeEnd("load persons");
-            if (error) console.log(error);
+            console.time("load persons");
+            d3.json(this.fileUrl, function (error, persons) {
+                console.timeEnd("load persons");
+                if (error) console.log(error);
 
-            var obj = persons;
-            thisThis.clearTable();
-            var table = $("#" + thisThis.idTable);
-            //table[0].border = "1";
-            var columns = Object.keys(obj[0]);
-            var columnCount = columns.length;
-            var row = $(table[0].insertRow(-1));
-            for (var i = 0; i < columnCount; i++) {
-                if (i == 0 || i == 1 || i == 2 || i == 5) {
-                    var headerCell = $("<th />");
-                    headerCell.html([columns[i]]);
-                    row.append(headerCell);
-                }
-            }
+                thisThis.data = persons;
+                thisThis.addDataToTable();
 
-            for (var i = 0; i < obj.length; i++) {
-                row = $(table[0].insertRow(-1));
-                row.addClass("hand-cursor");
-                for (var j = 0; j < columnCount; j++) {
-                    if (j == 0 || j == 1 || j == 2 || j == 5) {
-                        var cell = $("<td />");
-                        cell.html(obj[i][columns[j]]);
-                        row.append(cell);
+                /*var obj = persons;
+                thisThis.clearTable();
+                var table = $("#" + thisThis.idTable);
+                //table[0].border = "1";
+                var columns = Object.keys(obj[0]);
+                var columnCount = columns.length;
+                var row = $(table[0].insertRow(-1));
+                for (var i = 0; i < columnCount; i++) {
+                    if (i == 0 || i == 1 || i == 2 || i == 5) {
+                        var headerCell = $("<th />");
+                        headerCell.html([columns[i]]);
+                        row.append(headerCell);
                     }
                 }
-            }
-
-            document.querySelectorAll("#" + thisThis.idTable+" tr")
-                .forEach(e => e.addEventListener("click", thisThis.rowTableClickHandler));
-
-            //table.append(table);
-        });
+    
+                for (var i = 0; i < obj.length; i++) {
+                    row = $(table[0].insertRow(-1));
+                    row.addClass("hand-cursor");
+                    row.id = i;
+                    for (var j = 0; j < columnCount; j++) {
+                        if (j == 0 || j == 1 || j == 2 || j == 5) {
+                            var cell = $("<td />");
+                            cell.html(obj[i][columns[j]]);
+                            row.append(cell);
+                        }
+                    }
+                }
+    
+                document.querySelectorAll("#" + thisThis.idTable + " tr")
+                    .forEach(e => e.addEventListener("click", thisThis.rowTableClickHandler));
+    
+                //table.append(table);
+                */
+            });
+        }
+        else {
+            this.addDataToTable();
+        }
     }
 
 }  
