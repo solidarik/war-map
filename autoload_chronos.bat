c8f0cc3e5c5c757136fc4ec35b4807b2fe3c4aae
solidarik <ildar.gubaydullin@gmail.com>
2020-01-10 23:12:51 +0500

    Deleted chronos files

--- autoload_chronos.bat
+++ autoload_chronos.bat
@@ -1,6 +1,6 @@
 @echo off
 
 call python.exe loadDatabase\python\parse_xls_to_json_chronos.py
-call node.exe loadDatabase\load.js
+call node.exe loadDatabase\load.js > autoload.log
 
 echo Complited
