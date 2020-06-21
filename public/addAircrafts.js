f3ac22adb6705e7b7539d234c006673005dfe3ab
codmonkdev <codemonkeydeveloper@mail.ru>
2020-06-21 19:43:53 +0400

    Сравнение в одну строку, теперь нет сдвига

--- public/addAircrafts.js
+++ public/addAircrafts.js
@@ -16,122 +16,97 @@ var addAircrafts = /*#__PURE__*/function () {
   _createClass(addAircrafts, null, [{
     key: "addAircraft",
     value: function addAircraft(divId, aircraftData) {
-      var div = document.getElementById(divId);
-      div.innerHTML = "";
-      var newNode = document.createElement('div');
+      // var div = document.getElementById(divId);
+      // div.innerHTML = "";
+      var newNode = document.getElementById(divId + '-Img');
       newNode.innerHTML = "<img class='image-center img-rounded resized-image' id='imgPerson' src='" + aircraftData.URL_Local_picture + "' alt=''>";
       newNode.classList.add("row");
       newNode.classList.add("img");
-      newNode.classList.add("image-cont");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Тип - <p id='Type'>" + aircraftData.Type + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Вид - <p id='Kind'>" + aircraftData.Kind + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Название - <p id='Brand'>" + aircraftData.Brand + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Страна - <p id='Country'>" + aircraftData.Country + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Конструктор - <p id='Constructor'>" + aircraftData.Constructor + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Производитель - <p id='Manufacturer'>" + aircraftData.Manufacturer + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Дата создания - <p id='Year_creation'>" + aircraftData.Year_creation + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Принят на вооружение - <p id='Adopted'>" + aircraftData.Adopted + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Окончание производства - <p id='Production_end'>" + aircraftData.Production_end + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Количество - <p id='Number'>" + aircraftData.Number + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Вес, кг - <p id='Weight_kg'>" + aircraftData.Weight_kg + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Длина, м - <p id='Length_m'>" + aircraftData.Length_m + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = "Место производства - <p id='Production_place'>" + aircraftData.Production_place + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode); // newNode = document.createElement('div');      
-      // newNode.innerHTML = "Водоизмещение, т - <p id='Displacement_t'>"+aircraftData.Displacement_t+"</p>";
-      // newNode.classList.add("row");
-      // div.appendChild( newNode ); 
-      // newNode = document.createElement('div');      
-      // newNode.innerHTML = "Толщина брони, мм - <p id='Armor_thickness_mm_board'>"+aircraftData.Armor_thickness_mm_board+"</p>";
-      // newNode.classList.add("row");
-      // div.appendChild( newNode ); 
-      // newNode = document.createElement('div');      
-      // newNode.innerHTML = "Основной каллибр, мм - <p id='Main_caliber_mm'>"+aircraftData.Main_caliber_mm+"</p>";
-      // newNode.classList.add("row");
-      // div.appendChild( newNode ); 
-      // newNode = document.createElement('div');      
-      // newNode.innerHTML = "Боекомплект - <p id='Ammunition'>"+aircraftData.Ammunition+"</p>";
-      // newNode.classList.add("row");
-      // div.appendChild( newNode );         
-      // newNode = document.createElement('div');      
-      // newNode.innerHTML = "Скорострельность - <p id='Rate_of_fire'>"+aircraftData.Rate_of_fire+"</p>";
-      // newNode.classList.add("row");
-      // div.appendChild( newNode );    
-      // newNode = document.createElement('div');      
-      // newNode.innerHTML = "Дальность стрельбы, км - <p id='Firing_range_km'>"+aircraftData.Firing_range_km+"</p>";
-      // newNode.classList.add("row");
-      // div.appendChild( newNode );    
+      newNode.classList.add("image-cont"); //div.appendChild( newNode );
 
-      newNode = document.createElement('div');
+      var newNode = document.getElementById(divId + '-Type');
+      newNode.innerHTML = "Тип - <p id='Type'>" + aircraftData.Type + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );
+
+      var newNode = document.getElementById(divId + '-Kind');
+      newNode.innerHTML = "Вид - <p id='Kind'>" + aircraftData.Kind + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );
+
+      var newNode = document.getElementById(divId + '-Brand');
+      newNode.innerHTML = "Название - <p id='Brand'>" + aircraftData.Brand + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );
+
+      var newNode = document.getElementById(divId + '-Country');
+      newNode.innerHTML = "Страна - <p id='Country'>" + aircraftData.Country + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );        
+
+      var newNode = document.getElementById(divId + '-Constructor');
+      newNode.innerHTML = "Конструктор - <p id='Constructor'>" + aircraftData.Constructor + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Manufacturer');
+      newNode.innerHTML = "Производитель - <p id='Manufacturer'>" + aircraftData.Manufacturer + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Year_creation');
+      newNode.innerHTML = "Дата создания - <p id='Year_creation'>" + aircraftData.Year_creation + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Adopted');
+      newNode.innerHTML = "Принят на вооружение - <p id='Adopted'>" + aircraftData.Adopted + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );          
+
+      var newNode = document.getElementById(divId + '-Production_end');
+      newNode.innerHTML = "Окончание производства - <p id='Production_end'>" + aircraftData.Production_end + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );            
+
+      var newNode = document.getElementById(divId + '-Number');
+      newNode.innerHTML = "Количество - <p id='Number'>" + aircraftData.Number + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );         
+
+      var newNode = document.getElementById(divId + '-Weight_kg');
+      newNode.innerHTML = "Вес, кг - <p id='Weight_kg'>" + aircraftData.Weight_kg + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Length_m');
+      newNode.innerHTML = "Длина, м - <p id='Length_m'>" + aircraftData.Length_m + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Production_place');
+      newNode.innerHTML = "Место производства - <p id='Production_place'>" + aircraftData.Production_place + "</p>";
+      newNode.classList.add("row"); //div.appendChild( newNode ); 
+
+      var newNode = document.getElementById(divId + '-Speed_km_h_or_knots');
       newNode.innerHTML = "Скорость, км/ч - <p id='Speed_km_h_or_knots'>" + aircraftData.Speed_km_h_or_knots + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Crew_person');
       newNode.innerHTML = "Экипаж - <p id='Crew_person'>" + aircraftData.Crew_person + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Cruising_range_km');
       newNode.innerHTML = "Дальность полета, км - <p id='Cruising_range_km'>" + aircraftData.Cruising_range_km + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Engine_power_hp');
       newNode.innerHTML = "Мощность двигателя, л.с. - <p id='Engine_power_hp'>" + aircraftData.Engine_power_hp + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Armament');
       newNode.innerHTML = "Вооружение - <p id='Armament'>" + aircraftData.Armament + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Practical_ceiling');
       newNode.innerHTML = "Практический потолок - <p id='Practical_ceiling'>" + aircraftData.Practical_ceiling + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Source');
       newNode.innerHTML = "Источник - <p id='Source'><a href='" + aircraftData.Source + "'>Ссылка</a></p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Note');
       newNode.innerHTML = "Примечание - <p id='Note'>" + aircraftData.Note + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
+      newNode.classList.add("row"); //div.appendChild( newNode );  
     }
   }]);
 
--- public/addShips.js
+++ public/addShips.js
@@ -16,109 +16,105 @@ var addShips = /*#__PURE__*/function () {
   _createClass(addShips, null, [{
     key: "addShip",
     value: function addShip(divId, shipData) {
-      var div = document.getElementById(divId);
-      div.innerHTML = "";
-      var newNode = document.createElement('div');
+      // var div = document.getElementById(divId);
+      // div.innerHTML = "";
+      var newNode = document.getElementById(divId + '-Img');
       newNode.innerHTML = "<img class='image-center img-rounded resized-image' id='imgPerson' src='" + shipData.URL_Local_picture + "' alt=''>";
       newNode.classList.add("row");
       newNode.classList.add("img");
-      newNode.classList.add("image-cont");
-      div.appendChild(newNode); // <div class="row img image-cont">
-      //     <img class="image-center img-rounded resized-image" id="imgPerson" src="img/ships/Prince_of_Wales.jpg" alt="">
-      // </div>
+      newNode.classList.add("image-cont"); //div.appendChild( newNode );
 
-      newNode = document.createElement('div');
+      var newNode = document.getElementById(divId + '-Country');
       newNode.innerHTML = "Страна - <p id='Country'>" + shipData.Country + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );
+
+      var newNode = document.getElementById(divId + '-Ships_in_series');
       newNode.innerHTML = "Кораблей в серии - <p id='Ships_in_series'>" + shipData.Ships_in_series + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );
+
+      var newNode = document.getElementById(divId + '-Ship_type');
       newNode.innerHTML = "Тип корабля - <p id='Ship_type'>" + shipData.Ship_type + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );
+
+      var newNode = document.getElementById(divId + '-Name');
       newNode.innerHTML = "Название - <p id='Name'>" + shipData.Name + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );        
+
+      var newNode = document.getElementById(divId + '-Developer');
       newNode.innerHTML = "Разработчик - <p id='Developer'>" + shipData.Developer + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Place_of_construction');
       newNode.innerHTML = "Место строительства - <p id='Place_of_construction'>" + shipData.Place_of_construction + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Release_date');
       newNode.innerHTML = "Дата выпуска - <p id='Release_date'>" + shipData.Release_date + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Production_end');
       newNode.innerHTML = "Дата окончания эксплуатации - <p id='Production_end'>" + shipData.Production_end + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );          
+
+      var newNode = document.getElementById(divId + '-Displacement_t');
       newNode.innerHTML = "Водоизмещение, т - <p id='Displacement_t'>" + shipData.Displacement_t + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );            
+
+      var newNode = document.getElementById(divId + '-Place_of_the_main_battle');
       newNode.innerHTML = "Место основного сражения - <p id='Place_of_the_main_battle'>" + shipData.Place_of_the_main_battle + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );         
+
+      var newNode = document.getElementById(divId + '-Date_of_the_main_battle');
       newNode.innerHTML = "Дата основного сражения - <p id='Date_of_the_main_battle'>" + shipData.Date_of_the_main_battle + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Result_of_the_main_battle');
       newNode.innerHTML = "Результат основного сражения - <p id='Result_of_the_main_battle'>" + shipData.Result_of_the_main_battle + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Main_humming_force');
       newNode.innerHTML = "Основной колибр - <p id='Main_humming_force'>" + shipData.Main_humming_force + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode ); 
+
+      var newNode = document.getElementById(divId + '-Number_of_guns');
       newNode.innerHTML = "Число пушек - <p id='Number_of_guns'>" + shipData.Number_of_guns + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode ); 
+
+      var newNode = document.getElementById(divId + '-Crew');
       newNode.innerHTML = "Экипаж - <p id='Crew'>" + shipData.Crew + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode ); 
+
+      var newNode = document.getElementById(divId + '-Range_thousand_km');
       newNode.innerHTML = "Дальность хода, тыс. км - <p id='Range_thousand_km'>" + shipData.Range_thousand_km + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode ); 
+
+      var newNode = document.getElementById(divId + '-Engine_power_hp');
       newNode.innerHTML = "Мощность двигателя, л.с. - <p id='Engine_power_hp'>" + shipData.Engine_power_hp + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );         
+
+      var newNode = document.getElementById(divId + '-Length_m');
       newNode.innerHTML = "Длина, м - <p id='Length_m'>" + shipData.Length_m + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );    
+
+      var newNode = document.getElementById(divId + '-Sidewall_ammunition_mm');
       newNode.innerHTML = "Броня бортов, мм - <p id='Sidewall_ammunition_mm'>" + shipData.Sidewall_ammunition_mm + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );    
+
+      var newNode = document.getElementById(divId + '-Speed_knots');
       newNode.innerHTML = "Скорость, узлы - <p id='Speed_knots'>" + shipData.Speed_knots + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Firing_range_km');
       newNode.innerHTML = "Дальность стрельбы, км - <p id='Firing_range_km'>" + shipData.Firing_range_km + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Source');
       newNode.innerHTML = "Источник - <p id='Source'><a href='" + shipData.Source + "'>Ссылка</a></p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
-      newNode = document.createElement('div');
+      newNode.classList.add("row"); //div.appendChild( newNode );  
+
+      var newNode = document.getElementById(divId + '-Note');
       newNode.innerHTML = "Примечание - <p id='Note'>" + shipData.Note + "</p>";
-      newNode.classList.add("row");
-      div.appendChild(newNode);
+      newNode.classList.add("row"); //div.appendChild( newNode );  
     }
   }]);
 
