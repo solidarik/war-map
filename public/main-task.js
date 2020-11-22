53384a54b0794fc1d4457fed56e5c9bcc4759dad
codmonkdev <codemonkeydeveloper@mail.ru>
2020-11-22 21:48:04 +0400

    change hints and history days

--- public/main-task.js
+++ public/main-task.js
@@ -82,13 +82,20 @@ function startApp() {
     //persons.clearTable();
     //console.log('3');
     //persons.fillTable();
+    var options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    }
+    $('#Date').html('<h5>'+(new Date()).toLocaleDateString('ru-RU', options)+'</h5>');
 
     $("#persons-table tr:eq(0) td:first-child span").click();
 
     var urlHints = 'data/hints.json';
+    // console.log('data/hints.json - start load');
 
     d3.json(urlHints, function (error, hints) {
-
+      // console.log('data/hints.json - finish load');
       hints.sort(function() { return .5 - Math.random();});
 
       hintArr = hints;
@@ -105,74 +112,92 @@ function startApp() {
 
       });
 
-    });
+
+        var urlPersons = 'data/persons.json';
+        // console.log('data/persons.json - start load');
+        d3.json(urlPersons, function (error, persons) {
+          // console.log('data/persons.json - finish load');
+          if (error) console.log(error);
     
-    var urlPersons = 'data/persons.json';
-
-    d3.json(urlPersons, function (error, persons) {
-
-      var dtTemp = new Date();
-      var dt = new Date(dtTemp.getFullYear(),dtTemp.getMonth(),dtTemp.getDate());
-
-      var birthPersons=persons.filter(function(d) { 
-        if (typeof d.DateBirth=="string"&&d.DateBirth!=""&&d.DateBirth.indexOf('.')>-1){
-        var dtDateBirth = d.DateBirth; 
-        var datePartsBirth = dtDateBirth.split(".");
-
-        var dtObjectDateBirth = new Date(+datePartsBirth[2], datePartsBirth[1] - 1, +datePartsBirth[0]);
+          //var dtTemp = new Date();
+          //var dt = new Date(dtTemp.getFullYear(),dtTemp.getMonth(),dtTemp.getDate());
+          var dt = new Date();
+          dt.setHours(0,0,0,0);
+          var birthPersons=persons.filter(function(d) { 
+            if (typeof d.DateBirth=="string"&&d.DateBirth!=""&&d.DateBirth.indexOf('.')>-1){
+            var dtDateBirth = d.DateBirth; 
+            var datePartsBirth = dtDateBirth.split(".");
+              
+            var dtObjectDateBirth = new Date(+datePartsBirth[2], datePartsBirth[1] - 1, +datePartsBirth[0]);
+            dtObjectDateBirth.setUTCFullYear(+datePartsBirth[2]);
+            dtObjectDateBirth.setUTCMonth( datePartsBirth[1] - 1);
+            dtObjectDateBirth.setUTCDate(+datePartsBirth[0]);
+            dtObjectDateBirth.setHours(0,0,0,0);
+            // if(d.Surname=='Комаров'){
+            //     console.log('d.DateBirth='+d.DateBirth);
+            //     console.log('dt.toString='+dt.toString());
+            //     console.log('dtObjectDateBirth.toString='+dtObjectDateBirth.toString());
+            //     console.log('dt.getDate()='+dt.getDate());
+            //     console.log('dtObjectDateBirh.getDate()='+dtObjectDateBirth.getDate());
+            //     console.log('dt.getMonth='+dt.getMonth());
+            //     console.log('dtObjectDateBirth.getMonth='+dtObjectDateBirth.getMonth());   
+            // }       
+            return dt.getDate()==dtObjectDateBirth.getDate()&&dt.getMonth()==dtObjectDateBirth.getMonth();}
+            else{
+              return false;
+            }
+          });
         
-        //  console.log('dt.toString='+dt.toString());
-        //  console.log('dtObjectDateBirth.toString='+dtObjectDateBirth.toString());
-        //  console.log('dt.getDate()='+dt.getDate());
-        //  console.log('dtObjectDateBirh.getDate()='+dtObjectDateBirth.getDate());
-        //  console.log('dt.getMonth='+dt.getMonth());
-        //  console.log('dtObjectDateBirth.getMonth='+dtObjectDateBirth.getMonth());        
-        return dt.getDate()==dtObjectDateBirth.getDate()&&dt.getMonth()==dtObjectDateBirth.getMonth();}
-        else{
-          return false;
-        }
-      });
-    
-      //console.log(birthPersons);
-      $('#100YearsAgo').html('');
-      for(var i =0;i<birthPersons.length;i++) {
-          var k=birthPersons[i];
-          $('#100YearsAgo').html($('#100YearsAgo').html()+'В этот день '+k.DateBirth+' родился '+k.Surname+' '+k.Name+' '+k.MiddleName+'<br><a href="'+k.Source+'">Подробнее...</><br>');
-      }
-      
-      var deathPersons=persons.filter(function(d) { 
-        if (typeof d.DateDeath=="string"&&d.DateDeath!=""&&d.DateDeath.indexOf('.')>-1){
-         
-          var dtDateDeath = d.DateDeath; 
-          var datePartsDeath = dtDateDeath.split(".");
-
-          var dtObjectDateDeath = new Date(+datePartsDeath[2], datePartsDeath[1] - 1, +datePartsDeath[0]);
+          // console.log(birthPersons);
+          $('#100YearsAgo').html('');
+          for(var i =0;i<birthPersons.length;i++) {
+              var k=birthPersons[i];
+              $('#100YearsAgo').html($('#100YearsAgo').html()+'В этот день '+k.DateBirth+' родился '+k.Surname+' '+k.Name+' '+k.MiddleName+'<br><a href="'+k.Source+'">Подробнее...</><br>');
+          }
           
-          // console.log('dt.toString='+dt.toString());
-          // console.log('dtObjectDateBirth.toString='+dtObjectDateBirth.toString());
-          // console.log('dt.getDate()='+dt.getDate());
-          // console.log('dtObjectDateBirth.getDate()='+dtObjectDateBirth.getDate());
-          // console.log('dt.getMonth='+dt.getMonth());
-          // console.log('dtObjectDateBirth.getMonth='+dtObjectDateBirth.getMonth());        
-          return dt.getDate()==dtObjectDateDeath.getDate()&&dt.getMonth()==dtObjectDateDeath.getMonth();
-        }
-        else{
-          return false;
-        }
-
-      });
-      //console.log(deathPersons);
-      
-      for(var i =0;i<deathPersons.length;i++) {
-          var k=deathPersons[i];
-          $('#100YearsAgo').html($('#100YearsAgo').html()+'В этот день '+k.DateDeath+' умер '+k.Surname+' '+k.Name+' '+k.MiddleName+'<br><a href="'+k.Source+'">Подробнее...</><br>');
-      }      
-
-      if(deathPersons.length==0&&birthPersons.length==0){
-        $('#100YearsAgo').html('В этот день не было событий');
-      }
+          var deathPersons=persons.filter(function(d) { 
+            if (typeof d.DateDeath=="string"&&d.DateDeath!=""&&d.DateDeath.indexOf('.')>-1){
+            
+              var dtDateDeath = d.DateDeath; 
+              var datePartsDeath = dtDateDeath.split(".");
+    
+              var dtObjectDateDeath = new Date(+datePartsDeath[2], datePartsDeath[1] - 1, +datePartsDeath[0]);
+              dtObjectDateDeath.setUTCFullYear(+datePartsDeath[2]);
+              dtObjectDateDeath.setUTCMonth(datePartsDeath[1] - 1);
+              dtObjectDateDeath.setUTCDate(+datePartsDeath[0]);
+              dtObjectDateDeath.setHours(0,0,0,0);
+              // if(d.Surname=='Комаров'){
+              //   console.log('d.DateDeath='+d.DateDeath);
+              //   console.log('dt.toString='+dt.toString());
+              //   console.log('dtObjectDateDeath.toString='+dtObjectDateDeath.toString());
+              //   console.log('dt.getDate()='+dt.getDate());
+              //   console.log('dtObjectDateDeath.getDate()='+dtObjectDateDeath.getDate());
+              //   console.log('dt.getMonth='+dt.getMonth());
+              //   console.log('dtObjectDateDeath.getMonth='+dtObjectDateDeath.getMonth());       
+              // } 
+              return dt.getDate()==dtObjectDateDeath.getDate()&&dt.getMonth()==dtObjectDateDeath.getMonth();
+            }
+            else{
+              return false;
+            }
+    
+          });
+          // console.log(deathPersons);
+          
+          for(var i =0;i<deathPersons.length;i++) {
+              var k=deathPersons[i];
+              $('#100YearsAgo').html($('#100YearsAgo').html()+'В этот день '+k.DateDeath+' умер '+k.Surname+' '+k.Name+' '+k.MiddleName+'<br><a href="'+k.Source+'">Подробнее...</><br>');
+          }      
+    
+          if(deathPersons.length==0&&birthPersons.length==0){
+            $('#100YearsAgo').html('В этот день не было событий');
+          }
+    
+        });
 
     });
+    
+
 
 }
 
