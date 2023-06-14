1fb981af694ba62ef39391f1e63c1618dc530024
solidarik <ildar.gubaydullin@gmail.com>
2023-06-14 12:33:19 +0500

    page about remark

--- typescript/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const saveMaps_1 = __importDefault(require("./saveMaps"));
-const sClass = new saveMaps_1.default('../public/images/maps/');
-sClass.saveGMaps();
--- typescript/index.tsx
+++
@@ -1,4 +0,0 @@
-import SaveClass from './saveMaps'
-
-const sClass = new SaveClass('../public/images/maps/')
-sClass.saveGMaps()
